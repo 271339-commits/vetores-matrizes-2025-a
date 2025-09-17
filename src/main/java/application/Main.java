365355a6b64fd8c7873610de97969005bd675bba
271339-commits <271339@unifio.edu.br>
2025-09-17 01:33:34 +0000

    a

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import application.lista.ListaSimples;
+
 public class Main {
     public static void main(String[] args) {
         //Declaração de Vetores
@@ -7,6 +9,15 @@ public class Main {
 
         //Declaração de Matrizes
         int [][] matriz = new int [5][3];
+
+        //Declaração de listaSimples
+        ListaSimples lista = new ListaSimples();
+        lista.adicionar("Primeiro");
+        lista.adicionar("Segundo");
+        lista.adicionar("terceiro");
+        lista.remover(0);
+
+        System.out.println(lista);
     }
 }
 
